b6b98c11e4f1a1255fcf6397310342947d77872d
Paige Bolduc <paigebolduc@gmail.com>
2016-02-15 09:36:17 -0800

    add npm and bower dependencies, create module.js file for angular config

---
+++ public/js/module.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var app = angular.module("toDo", ["ui.router"]);
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+    .state("home", {url: "/", templateUrl: "/partials/home.html", controller: "homeCtrl"})
+    .state("addTask", {url: "/addTask", templateUrl: "/partials/addTask.html", controller: "addCtrl"})
+
+  $urlRouterProvider.otherwise("/");
+});
