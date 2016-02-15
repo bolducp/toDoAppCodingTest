c5bc80db428333ff86a32ccf68da85099054e5c9
Paige Bolduc <paigebolduc@gmail.com>
2016-02-15 10:26:54 -0800

    working on the homeCtrl and Tasks service to make http requests to the back end, which queries the database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var Task = require('../models/task');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'MEAN To Do App' });
 });
+
+router.get('/tasks', function(req, res, next) {
+  Task.find({}, function(err, tasks){
+    if (err) return res.status(400).send(err);
+    res.send(tasks);
+  })
+});
+
 
 module.exports = router;
